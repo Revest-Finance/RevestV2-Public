e1ab3800f4b4ec3180a8701845a2cb6cd8a5dc76
thanhtrinh2003 <thanhtrinh280506@gmail.com>
2023-05-16 12:42:42 -0500

    fix erc1155 callback function

--- src/Revest_base.sol
+++ src/Revest_base.sol
@@ -214,6 +214,26 @@ abstract contract Revest_base is IRevest, ReentrancyGuard, Ownable {
         smartWallet = tokenVault.getAddress(walletSalt, address(this));
     } 
 
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) external returns (bytes4) {
+        return bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"));
+    }
+
+    function onERC1155BatchReceived(
+        address operator,
+        address from,
+        uint256[] calldata _ids,
+    uint256[] calldata _values,
+        bytes calldata data
+    ) external returns (bytes4) {
+        return bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"));
+    }
+
     receive() external payable {
         //Do Nothing but receive
     }
