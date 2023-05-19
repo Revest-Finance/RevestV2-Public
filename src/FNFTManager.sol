b44c0c607461757f8f4f00a726277b2d0185df52
thanhtrinh2003 <thanhtrinh280506@gmail.com>
2023-05-19 09:09:49 -0500

    Fix SVG Generator + FNFT Manager Token URI

--- src/FNFTManager.sol
+++ src/FNFTManager.sol
@@ -1,11 +1,53 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.14;
 
+import "solmate/tokens/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+import "./lib/FNFTRenderer.sol";
 
-contract FNFTManager is ERC721 {
+contract RevestNFTManager is ERC721 {
     address public immutable factory;
+    uint256 private nextTokenId;
+    uint256 public totalSupply;
+    mapping(uint256 => AddressLock) public lockList;
+
+    event AddAddressLock(
+        uint fnftId,
+        uint amount,
+        address tokenAddress,
+        address unlockAddress
+    );
+
+    struct AddressLock {
+        uint tokenId; //TODO: added indexed 
+        string assetName;
+        string assetTicker;
+        uint amount;
+        address tokenAddress;
+        address unlockAddress;
+    }
+
+    function mintAddressLock(
+        uint amount, 
+        address tokenAddress, 
+        address unlockAddress
+    ) public returns (uint256 tokenId) {
+        tokenId = nextTokenId++;
+        _mint(unlockAddress, tokenId);
+        totalSupply++;
+
+        string memory assetName = ERC20(tokenAddress).name();
+        string memory assetTicker = ERC20(tokenAddress).symbol();
+
+
+        AddressLock memory currentLock = AddressLock(tokenId, assetName, assetTicker, amount, tokenAddress, unlockAddress);
+        lockList[tokenId] = currentLock;
+        emit AddAddressLock(tokenId, amount, tokenAddress, unlockAddress);
+    }
 
     constructor(address factoryAddress)
-        ERC721("UniswapV3 NFT Positions", "UNIV3")
+        ERC721("Revest FNFT Lock", "FNFT")
     {
         factory = factoryAddress;
     }
@@ -16,6 +58,18 @@ contract FNFTManager is ERC721 {
         override
         returns (string memory)
     {
-        return "";
+        AddressLock memory addressLock = lockList[tokenId];
+
+        //TODO: fix the return NFT 
+        return
+            FNFTRenderer.render(
+                FNFTRenderer.RenderParams({
+                    assetName: addressLock.assetName,
+                    assetTicker: addressLock.assetTicker,
+                    amount: addressLock.amount,
+                    lockType: "address lock",
+                    unlockAddress: addressLock.unlockAddress
+                })
+            );
     }
 }
--- src/lib/FNFTRenderer.sol
+++ src/lib/FNFTRenderer.sol
@@ -15,9 +15,6 @@ library FNFTRenderer {
         address unlockAddress;
     }
 
-
-
-
     function render(RenderParams memory param) internal view returns (string memory) {
         string memory image = string.concat(
             "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 300 480'>",
